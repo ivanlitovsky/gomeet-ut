726111dbadb51e3263574a40bbcb625135f685f3
ivanlitovsky <ivan.litovsky@gmail.com>
2024-01-08 11:12:36 +0100

    update

--- tools.py
+++ tools.py
@@ -4,9 +4,9 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate("firebase.json")
+
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
 
